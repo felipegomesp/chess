f0f69a9d6ac8668eb51fd1b7edd443c10004308c
Felipe Gomes <felipe.gomes.p@gmail.com>
2020-04-03 12:00:45 -0300

    Printing possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,28 +14,29 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Board board = new Board(8, 8);
-		
+
 		ChessMatch chessMatch = new ChessMatch();
-		
+
 		while (true) {
 			try {
-			UI.clearScreen();
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println();
-			System.out.print("Source: ");
-			ChessPosition source = UI.readChessPosition(sc);
-			
-			System.out.println();
-			System.out.print("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
-			
-			ChessPiece capturedChessPiece = chessMatch.performChessMove(source, target);
-			}
-			catch (ChessException e) {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+
+				ChessPiece capturedChessPiece = chessMatch.performChessMove(source, target);
+			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.hasNextLine();
-			}
-			catch (InputMismatchException e) {
+			} catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.hasNextLine();
 			}
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -15,14 +15,14 @@ public class Rook extends ChessPiece {
 	public String toString() {
 		return "R";
 	}
-	
+
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-		
+
 		Position p = new Position(0, 0);
-		
-		//above
+
+		// above
 		p.setValues(position.getRow() - 1, position.getColumn());
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
@@ -31,9 +31,9 @@ public class Rook extends ChessPiece {
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
-		
-		//left
-		p.setValues(position.getRow(), position.getColumn() -1);
+
+		// left
+		p.setValues(position.getRow(), position.getColumn() - 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setColumn(p.getColumn() - 1);
@@ -41,9 +41,9 @@ public class Rook extends ChessPiece {
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
-		
-		//right
-		p.setValues(position.getRow(), position.getColumn() +1);
+
+		// right
+		p.setValues(position.getRow(), position.getColumn() + 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setColumn(p.getColumn() + 1);
@@ -51,19 +51,18 @@ public class Rook extends ChessPiece {
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
-		
-		//below
-				p.setValues(position.getRow() + 1, position.getColumn());
-				while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
-					mat[p.getRow()][p.getColumn()] = true;
-					p.setRow(p.getRow() + 1);
-				}
-				if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
-					mat[p.getRow()][p.getColumn()] = true;
-				}
-		
+
+		// below
+		p.setValues(position.getRow() + 1, position.getColumn());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
 		return mat;
 	}
-	
-	
+
 }
