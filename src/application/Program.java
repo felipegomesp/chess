6eee28b376a8b1b2873c2fa1b51a3d0a192a2c35
Felipe Gomes <felipe.gomes.p@gmail.com>
2020-04-07 00:17:16 -0300

    Handling captured pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,10 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
-import boardgame.Board;
 import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
@@ -12,35 +13,40 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
-		Board board = new Board(8, 8);
-
 		ChessMatch chessMatch = new ChessMatch();
-
+		List<ChessPiece> captured = new ArrayList<>();
+		
 		while (true) {
 			try {
 				UI.clearScreen();
-				UI.printMatch(chessMatch);
+				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-
+				
 				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-
-				ChessPiece capturedChessPiece = chessMatch.performChessMove(source, target);
-			} catch (ChessException e) {
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				
+				if (capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
+			}
+			catch (ChessException e) {
 				System.out.println(e.getMessage());
-				sc.hasNextLine();
-			} catch (InputMismatchException e) {
+				sc.nextLine();
+			}
+			catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
-				sc.hasNextLine();
+				sc.nextLine();
 			}
 		}
 	}
-
 }
